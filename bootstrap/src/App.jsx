2a41ad8cab0d051fdd4e6c89da4b31e9de52ee05
Vivek6034 <viveksingh6034@gmail.com>
2025-03-11 18:05:02 +0530

    boostrap

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,28 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+
+function App() {
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col>
+            <h1>Name</h1>
+            </Col>
+            <Col>
+            <h1>Age</h1>
+            </Col>
+            <Col>
+            <h1>Contact</h1>
+            </Col>
+            <Col>
+            <h1>City</h1>
+            </Col>
+        </Row>
+
+    </Container>
+
+    </>
+  )
+}
+export default App
